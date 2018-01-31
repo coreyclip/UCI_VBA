3acaafb0249accac6c7976e446cc603e891bc7e5
corey clippinger <coreyclip@gmail.com>
2018-01-30 16:14:42 -0800

    finished percent_change.vb

--- percent_change.vb
+++ percent_change.vb
@@ -3,17 +3,25 @@ Sub percent_change():
     ' and insert it in as a new column
     ' loop through each worksheet in Worksheets
     For Each ws in Worksheets 
-
+        'create column heading for percent change 
+        cells(1,9).value = "<percent change>"
         ' loop through Yearly Change column and opening price to calculate % change
         For i = 2 to ActiveSheet.Cells(ActiveSheet.Rows.Count, 2).End(xlUp).row
-            ' create variable  change as a percentage, 
-            dim chg as percent 
             ' bring in opening price as a double 
             dim opn as Double
-            opn = cells(i,) 
-            ' bring in yearly change in absolute terms 
+            opn = cells(i,3).value 
 
+            ' bring in yearly change in absolute terms
+            dim yr as double 
+            yr = cells(i,8).value 
+
+            ' create variable  change as a percentage, 
+            dim chg as percent 
+
+            chg = yr / opn 
+
+            cells(i,9).value = chg 
         next i 
-
+    next ws 
 
 end sub 
