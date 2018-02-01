2f83468df4575c404c96d384d409906a6b2d7018
corey clippinger <coreyclip@gmail.com>
2018-01-31 18:05:36 -0800

    adjusted lines 66 to 69 so that it no longer overwrites previous entries

--- moderate.vb
+++ moderate.vb
@@ -1,4 +1,3 @@
-
 Sub Summarize():
 
     ' Specify the location of the summary sheet
@@ -17,20 +16,24 @@ Sub Summarize():
     ' loop through each worksheet in Worksheets
     For Each ws In Worksheets
         ' create a last row variable
+        MsgBox(ws.name)
         
         LastRow = ws.Cells(ws.Rows.Count, 2).End(xlUp).Row
         MsgBox(LastRow)
         'create holder for total volume
-        Dim total As Long
+        Dim total As Double
         total = 0
 
+        'set initial row position for where opening prices is
+        dim row_break as double
+        row_break = 2 
+
+        ' create a variable for opening price
+        Dim opn As Double
+        opn = ws.cells(row_break,3).Value
+        'MsgBox(opn) 
         For i = 2 To LastRow
-            ' create a variable for opening price
-            Dim opn As Double
-            ' create a variable for closing price
-            Dim cls As Double
-            ' create a variable for change
-            Dim chg As Double
+           
 
             ' create variable for percent change
             Dim percent_change As Double
@@ -38,36 +41,42 @@ Sub Summarize():
             'check and see if the next row in the ticker column is the same
             'as the previous ticker
             If ws.Cells(i, 1).Value <> ws.Cells(i + 1, 1).Value Then
+                MsgBox("New Ticker "& ws.Cells(i + 1, 1).Value )
+                'collect closing price 
+                cls = ws.Cells(i, 6).Value
+                ' create a variable for change
+                Dim chg As Double
+
+                chg = opn - cls 
+                'calculate percent change
+                percent_change = chg / opn
+                'set new position of opening price
+                'MsgBox(i)
+                row_break = row_break + i 
                 'create a variable for the ticker
                 Dim ticker As String
                 ticker = ws.Cells(i, 1).Value
+
                
                 With summary_sheet
                     'last row of the summary table
-                     nextrow = .Cells(Rows.Count, 1).End(xlUp).Row
-                     MsgBox(nextrow)
-                    .Range("I2:I" & nextrow) = ticker
-                    .Range("J2:J" & nextrow) = chg
-                    .Range("K2:K" & nextrow) = percent_change
-                    .Range("L2:J" & nextrow) = total
+                     nextrow = .Cells(.Rows.Count, 9).End(xlUp).Row 
+                     'MsgBox(nextrow)
+
+                    .Range("I" & nextrow + 1) = ticker
+                    .Range("J" & nextrow + 1) = chg
+                    .Range("K" & nextrow + 1) = percent_change
+                    .Range("J" & nextrow + 1) = total
                 End With
+                ' set total back to zero 
                 total = 0
+                'set new opening price 
+                opn = ws.Cells(i+1, 3).value 
             Else
-                ' calculate out the Yearly change from what the stock opened
-                ' the year at to what the closing price was.
-                
-                opn = ws.Cells(i, 3).Value
-                cls = ws.Cells(i, 6).Value
-                
-                chg = cls - opn
 
-                'calcualte percent change
-                percent_change = chg / opn
-
-                'calculate total stock volume
-            
+                'tabulate total stock volume
                 total = total + ws.Cells(i, 7).Value
-            End If
+            end if 
         Next i
     Next ws
 
