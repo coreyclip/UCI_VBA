b1f7e2d378009cad2a04af3c6b13d3b2246b58c6
corey clippinger <coreyclip@gmail.com>
2018-02-02 12:45:31 -0800

    adjusted how opening and closing prices are calculated by using Min and Max functions within a range set by the beginning and ending of ticker row values

--- moderate.vb
+++ moderate.vb
@@ -7,83 +7,58 @@ Sub Summarize():
     'set column headers for summary tables
 
     
-    summary_sheet.Cells(1, 9).Value = "Ticker"
-    summary_sheet.Cells(1, 10).Value = "Yearly Change"
-    summary_sheet.Cells(1, 11).Value = "Percent Change"
-    summary_sheet.Cells(1, 12).Value = "Total Stock Volumne"
+    summary_sheet.cells(1,8).Value = "Ticker"
+    summary_sheet.cells(1,9).Value = "Yearly Change"
+    summary_sheet.cells(1,10).Value = "Percent Change"
+    summary_sheet.cells(1,11).Value = "Total Stock Volumne"
 
 
     ' loop through each worksheet in Worksheets
-    For Each ws In Worksheets
+    For Each ws in Worksheets
         ' create a last row variable
-        'MsgBox(ws.name)
-        
-        LastRow = ws.Cells(ws.Rows.Count, 2).End(xlUp).Row
-        'MsgBox(LastRow)
-        'create holder for total volume
-        Dim total As Double
+        With ActiveSheet
+            LastRow = .Cells(.Rows.Count, 2).End(xlUp).row
+        End With
+        'create holder for total volume 
+        dim total as long 
         total = 0
-
-        'set initial row position for where opening prices is
-        dim row_break as double
-        row_break = 2 
-
-        ' create a variable for opening price
-        Dim opn As Double
-        opn = ws.cells(row_break,3).Value
-        'MsgBox(opn) 
-        For i = 2 To LastRow
-           
-
-            ' create variable for percent change
-            Dim percent_change As Double
-
-            'check and see if the next row in the ticker column is the same
-            'as the previous ticker
-            If ws.Cells(i, 1).Value <> ws.Cells(i + 1, 1).Value Then
-               ' MsgBox("New Ticker "& ws.Cells(i + 1, 1).Value )
-                'collect closing price 
-                cls = ws.Cells(i, 6).Value
+        dim date_start as Double
+        date_start = 2 'first row index of date
+        for i = 2 to LastRow
+            dim ticker as string
+            ticker = ws.cells(i,1).Value 
+            if ticker <> ws.cells(i+1,1).value then
+                ' calculate out the Yearly change from what the stock opened
+                ' the year at to what the closing price was.
+                
+                ' create a variable for opening price
+                Dim opn As Double
+                ' create a variable for closing price
+                Dim cls As Double
                 ' create a variable for change
                 Dim chg As Double
-                chg = opn - cls 
-                'calculate percent change
-                if (chg <> 0) Then 
-                    percent_change = chg / opn 
-                else
-                    percent_change = 0
-                end if 
-                'set new position of opening price
-                'MsgBox(i)
-                row_break = row_break + i 
-                'create a variable for the ticker
-                Dim ticker As String
-                ticker = ws.Cells(i, 1).Value
 
-               
+                Set date_rng = Sheet1.Range("B" & date_start & ":B" & i)
+
+                opn = WorksheetFunction.Max(date_rng)
+                cls = WorksheetFunction.Min(date_rng) 
+                
+                chg = ws.cells(opn,3).value - ws.cells(cls, 6).value 
+                'calculate total stock volume
+                nextrow = summary_sheet.Range("I1").SpecialCells(xlCellTypeLastCell).Row
                 With summary_sheet
-                    'last row of the summary table
-                     nextrow = .Cells(.Rows.Count, 9).End(xlUp).Row 
-                     'MsgBox(nextrow)
-
-                    .Range("I" & nextrow + 1) = ticker
-                    .Range("J" & nextrow + 1) = chg
-                    .Range("K" & nextrow + 1) = percent_change
-                    .Range("L" & nextrow + 1) = total
+                    .Range("I2:I" & nextrow) = Ticker
+                    .Range("J2:J" & nextrow) = chg  
+                    .Range("L2:J" & nextrow) = total
                 End With
-                ' set total back to zero 
-                total = 0
-                'set new opening price 
-                opn = ws.Cells(i+1, 3).value 
-            Else
-
-                'tabulate total stock volume
-                total = total + ws.Cells(i, 7).Value
-            end if 
-        Next i
-    Next ws
-
-
+                'incriment start date
+                date_start = date_start + i 
+            else
+                'incriment the total stock volume
+                total = total + ws.cells(i,7).value
+            End If 
+        Next i 
+    Next ws     
 End Sub
         
 
